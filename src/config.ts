a0950e1b4df588bedeb199d932ff119ad7bfcfd6
Henrique Alexandre <Henriquealexandredec@gmail.com>
2024-09-16 21:40:01 -0300

    :sparkles: added more changes
    Signed-off-by: Henrique Alexandre <Henriquealexandredec@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,37 +1,37 @@
 import type { PostHideElements } from '~/content/config';
 
 export const Site = 'https://astro-blog-zozo.pages.dev';
-export const SiteLanguage = 'zh';
-export const SiteTitle = '无尽光芒';
-export const SiteDescription = '踏歌长行，梦想永在';
-export const FooterDescription = 'Be Nice Everyday';
+export const SiteLanguage = 'en';
+export const SiteTitle = 'Henalecam';
+export const SiteDescription = 'Just another blog';
+export const FooterDescription = 'Be nice more than clever';
 export const AdminName = 'Henalecam';
 export const PageSize = 15;
 
 // socialPlatform => userName
 // check components/Header.astro socialConfig for more info
 export const Socials: Record<string, Record<string, string>> = {
-	mail: { url: 'mailto:i@ladit.me' },
-	github: { url: 'https://github.com/ladit/astro-blog-zozo' },
-	x: { url: 'https://x.com/userName' },
-	mastodon: { url: 'https://mastodon.social/@userName' },
-	facebook: { url: 'https://facebook.com/userName' },
-	instagram: { url: 'https://instagram.com/userName' },
-	telegram: { url: 'https://t.me/@userName' },
-	youtube: { url: 'https://youtube.com/@userName' },
-	skype: { url: 'https://skype.com/userName' },
-	slack: { url: 'https://slack.com/userName' },
-	messenger: { url: 'https://messenger.com/userName' },
-	whatsapp: { url: 'https://whatsapp.com/userName' },
-	snapchat: { url: 'https://snapchat.com/userName' },
-	line: { url: 'https://line.com/userName' },
-	twitch: { url: 'https://twitch.com/userName' },
-	weibo: { url: 'https://weibo.com/userName' },
-	bilibili: { url: 'https://space.bilibili.com/userName' },
-	dingding: { url: 'https://dingtalk.com/userName' },
-	zhihu: { url: 'https://zhihu.com/userName' },
-	douban: { url: 'https://douban.com/userName' },
-	rss: { url: '/rss.xml' },
+	mail: { url: 'mailto:henriquealexandredec@gmail.com' },
+	github: { url: 'https://github.com/henalecam' },
+	// x: { url: 'https://x.com/userName' },
+	// mastodon: { url: 'https://mastodon.social/@userName' },
+	// facebook: { url: 'https://facebook.com/userName' },
+	instagram: { url: 'https://instagram.com/henalecam' },
+	// telegram: { url: 'https://t.me/@userName' },
+	// youtube: { url: 'https://youtube.com/@userName' },
+	// skype: { url: 'https://skype.com/userName' },
+	// slack: { url: 'https://slack.com/userName' },
+	// messenger: { url: 'https://messenger.com/userName' },
+	// whatsapp: { url: 'https://whatsapp.com/userName' },
+	// snapchat: { url: 'https://snapchat.com/userName' },
+	// line: { url: 'https://line.com/userName' },
+	twitch: { url: 'https://twitch.com/henalecam' },
+	// weibo: { url: 'https://weibo.com/userName' },
+	// bilibili: { url: 'https://space.bilibili.com/userName' },
+	// dingding: { url: 'https://dingtalk.com/userName' },
+	// zhihu: { url: 'https://zhihu.com/userName' },
+	// douban: { url: 'https://douban.com/userName' },
+	// rss: { url: '/rss.xml' },
 };
 
 // doc: https://giscus.app
